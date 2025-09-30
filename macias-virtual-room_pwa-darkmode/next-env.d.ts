9c92d884688efefbc0307e689b216af18dbb54db
londepan <lm@maciasspecialty.com>
2025-09-30 06:47:27 -0500

    Add files via upload

---
+++ macias-virtual-room_pwa-darkmode/next-env.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+/** Add type definitions for any custom stuff here */
---
+++ macias-virtual-room_pwa-darkmode/tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss"
+
+const config: Config = {
+  darkMode: 'class',
+  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        macias: {
+          DEFAULT: "#f36f21",
+          light: "#fde7d7",
+          dark: "#c94e0f"
+        }
+      },
+      borderColor: {
+        macias: "#f36f21"
+      }
+    }
+  },
+  plugins: []
+}
+export default config
